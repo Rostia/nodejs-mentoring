334773862eeeaec11514e073d6cb1da31324457d
Rostislav <roxan.net@mail.ru>
2021-03-23 10:15:03 +0300

    local fix bug

--- 1-module/task2.js
+++ 1-module/task2.js
@@ -4,13 +4,18 @@ import path from 'path';
 
 const readStream = fs.createReadStream(path.join(__dirname, '/csv', '/nodejs-hw1-ex1.csv'));
 const writeStream= fs.createWriteStream(path.join(__dirname, '/txt', '/nodejs-hw1-ex1.txt'));
+const converter = csv({
+  headers: ['book', 'author', 'amount', 'price'],
+  includeColumns: /book|author|price/
+});
 
-readStream.pipe(csv()).pipe(writeStream);
+readStream.pipe(converter).pipe(writeStream);
 
 readStream.on('error', (err) => {
   console.log('Error in read stream... \n');
   console.log(err);
 });
+
 writeStream.on('error', (err) => {
   console.log('Error in write stream... \n');
   console.log(err);
